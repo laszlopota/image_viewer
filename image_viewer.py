34416069d5e16b25baefd048a3e919122d695cb0
laszlopota <laszlopota00@gmail.com>
2022-01-22 07:59:26 +0100

    22/01/22/0759

--- image_viewer.py
+++ image_viewer.py
@@ -20,10 +20,6 @@ imagesList = []
 imagesPhotoList = []
 def open_images():
     global imagesPath, imagesNames, imagesList, imagesPhotoList
-    imagesPath = []
-    imagesNames = []
-    imagesList = []
-    imagesPhotoList = []
 
     file_path = filedialog.askopenfilenames()
     for image in file_path:
@@ -41,55 +37,44 @@ def open_images():
         imagesPhotoList.append(ImageTk.PhotoImage(imagesList[-1]))
 
     if len(imagesList) != 0:
-        resize_images()
-        show_first_image()
+        resize_image()
+        change_image()
 
 # Resizing images
-def resize_images():
-    global imagesList, imagesPhotoList
-    for i in range(len(imagesList)):
-        currentImageWidth, currentImageHeight = imagesList[i].size
-        newImageHeight = root.winfo_height() - 50
-        newImageWidth = currentImageWidth * newImageHeight / currentImageHeight
-        imagesList[i] = imagesList[i].resize((int(newImageWidth), int(newImageHeight)))
-        imagesPhotoList[i] = ImageTk.PhotoImage(imagesList[i])
+def resize_image():
+    global imagesList, imagesPhotoList, imageIndex
+    currentImageWidth, currentImageHeight = imagesList[imageIndex].size
+    newImageHeight = root.winfo_height() - 50
+    newImageWidth = currentImageWidth * newImageHeight / currentImageHeight
+    imagesList[imageIndex] = imagesList[imageIndex].resize((int(newImageWidth), int(newImageHeight)))
+    imagesPhotoList[imageIndex] = ImageTk.PhotoImage(imagesList[imageIndex])
 
 
 # Creating the image viewer
 imageIndex = 0
-def show_first_image():
-    global imagesPath, imagesNames, imagesList, imageIndex, imageShow, nameLabel
-    imageShow.grid_forget()
-    imageShow = Label(
+def change_image():
+    global imageLabel, nameLabel, imagesPhotoList, imageIndex, imagesNames
+
+    resize_image()
+    imageLabel.grid_forget()
+    imageLabel = Label(
         root, image=imagesPhotoList[imageIndex], background='white', width=root.winfo_width(),
         height=root.winfo_height() - 50, border=0
     )
-    imageShow.grid(row=0, column=0, columnspan=3)
+    imageLabel.grid(row=0, column=0, columnspan=3)
     nameLabel['text'] = imagesNames[imageIndex]
 
 def previous_image():
-    global imagesPath, imagesNames, imagesList, imageIndex, imageShow, nameLabel
+    global imageIndex
     if imageIndex != 0:
-        imageShow.grid_forget()
         imageIndex -= 1
-        imageShow = Label(
-            root, image=imagesPhotoList[imageIndex], background='white', width=root.winfo_width(),
-            height=root.winfo_height() - 50, border=0
-        )
-        imageShow.grid(row=0, column=0, columnspan=3)
-        nameLabel['text'] = imagesNames[imageIndex]
+        change_image()
 
 def next_image():
-    global imagesPath, imagesNames, imagesList, imageIndex, imageShow, nameLabel
+    global imageIndex, imagesList
     if imageIndex != len(imagesList)-1 and imageIndex != len(imagesList):
-        imageShow.grid_forget()
         imageIndex += 1
-        imageShow = Label(
-            root, image=imagesPhotoList[imageIndex], background='white', width=root.winfo_width(),
-            height=root.winfo_height() - 50, border=0
-        )
-        imageShow.grid(row=0, column=0, columnspan=3)
-        nameLabel['text'] = imagesNames[imageIndex]
+        change_image()
 
 
 # Start page and base widgets
@@ -97,10 +82,10 @@ plusImage = Image.open('plus.jpg')
 plusImage = plusImage.resize((150, 150))
 plusImage = ImageTk.PhotoImage(plusImage)
 
-imageShow = Label(
+imageLabel = Label(
     root, image=plusImage, background='white', width=root.winfo_width(), height=root.winfo_height() - 50, border=0
 )
-imageShow.grid(row=0, column=0, columnspan=3)
+imageLabel.grid(row=0, column=0, columnspan=3)
 nameLabel = Label(
     root, text='Select images!', width=int(root.winfo_width()/20), background='white', font=('Helvetica', 12)
 )
@@ -113,34 +98,29 @@ buttonNext = Button(root, text='>>', border=0, width=20, height=2, background='w
 buttonNext.grid(row=1, column=2)
 
 # Controls
-imageShow.bind('<Button-1>', lambda event: open_images())
+imageLabel.bind('<Button-1>', lambda event: open_images())
 root.bind('<Up>', lambda event: next_image())
 root.bind('<Right>', lambda event: next_image())
 root.bind('<Down>', lambda event: previous_image())
 root.bind('<Left>', lambda event: previous_image())
 
 def on_resize():
-    global imageShow, nameLabel, buttonPrevious, buttonNext
+    global imageLabel, nameLabel, buttonPrevious, buttonNext
     global imagesPhotoList, imagesList, imagesNames, imageIndex
-    imageShow.grid_forget()
+    imageLabel.grid_forget()
     if len(imagesList) != 0:
-        resize_images()
-        imageShow = Label(
+        resize_image()
+        imageLabel = Label(
             root, image=imagesPhotoList[imageIndex], background='white', width=root.winfo_width(),
             height=root.winfo_height() - 50, border=0
         )
-        nameLabel['text'] = imagesNames[imageIndex]
     else:
-        imageShow = Label(
+        imageLabel = Label(
             root, image=plusImage, background='white', width=root.winfo_width(), height=root.winfo_height() - 50,
             border=0
         )
-        imageShow.bind('<Button-1>', lambda event: open_images())
-        nameLabel['text'] = 'Select images!'
-    imageShow.grid(row=0, column=0, columnspan=3)
-    nameLabel.grid(row=1, column=1)
-    buttonPrevious.grid(row=1, column=0)
-    buttonNext.grid(row=1, column=2)
+        imageLabel.bind('<Button-1>', lambda event: open_images())
+    imageLabel.grid(row=0, column=0, columnspan=3)
 
 
 root.bind('<Motion>', lambda event: on_resize())
